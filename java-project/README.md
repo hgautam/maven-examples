9c5f0baa3cc690894e92b5a5edaae2529c226e0e
Himanshu Gautam <himanshu@aplimasolutions.com>
2020-04-18 19:53:31 -0700

    simple java project

---
+++ java-project/README.md
@@ -0,0 +1,15 @@
+# Maven – How to create a Java project
+Maven 3, Apache Commons Codec, a simple Java project to hash a string with the SHA-256 algorithm.
+
+
+## How to run this project?
+```
+$ git clone https://github.com/hgautam/maven-examples.git
+$ cd java-project
+$ mvn package 
+$ java -jar target/java-project-1.0-SNAPSHOT.jar 123456
+```
+Output
+```
+8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92
+```
---
+++ java-project/src/test/java/com/hgautam/hashing/AppTest.java
@@ -0,0 +1,21 @@
+package com.hgautam.hashing;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    private String INPUT = "123456";
+
+    @Test
+    public void testLength() {
+        Assert.assertEquals(64, App.sha256hex(INPUT).length());
+    }
+
+    @Test
+    public void testHex() {
+        String expected = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92";
+        Assert.assertEquals(expected, App.sha256hex(INPUT));
+    }
+
+}
