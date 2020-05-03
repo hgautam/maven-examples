384e3ceea5cb3bb8273397ff97171dec7951a134
Himanshu Gautam <himanshu@aplimasolutions.com>
2020-05-02 17:28:59 -0700

    fix error

--- maven-code-coverage/src/test/java/com/hgautam/examples/TestMessageBuilder.java
+++ maven-code-coverage/src/test/java/com/hgautam/examples/TestMessageBuilder.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestMessageBuilder {
 
     @Test
-    public void testNameMkyong() {
+    public void testNameHgautam() {
 
         MessageBuilder obj = new MessageBuilder();
         assertEquals("Hello hgautam", obj.getMessage("hgautam"));
