559c262633f5703552891b3625108b394ae993be
Himanshu Gautam <1302399+hgautam@users.noreply.github.com>
2020-10-02 15:28:55 -0700

    Update TestWelcome.java

--- java-multi-modules/web/src/test/java/com/hgautam/web/TestWelcome.java
+++ java-multi-modules/web/src/test/java/com/hgautam/web/TestWelcome.java
@@ -1,24 +1,28 @@
 package com.hgautam.web;
 
-import com.hgautam.web.config.SpringConfig;
+//import com.hgautam.web.config.SpringConfig;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import com.hgautam.web.config.*;
 
 //https://docs.spring.io/spring-framework/docs/current/spring-framework-reference/testing.html#integration-testing-annotations-junit-jupiter
-//@ExtendWith(SpringExtension.class)
-//@WebAppConfiguration
-//@ContextConfiguration(classes = SpringConfig.class)
-@SpringJUnitWebConfig(SpringConfig.class)
-@DisplayName("Test Spring MVC default view")
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = SpringConfig.class)
+//@SpringJUnitWebConfig(SpringConfig.class)
 public class TestWelcome {
 
     private MockMvc mockMvc;
@@ -32,17 +36,21 @@ public class TestWelcome {
     }
 
     @Test
-    public void testDefault() throws Exception {
+    public void testWelcome() throws Exception {
 
         this.mockMvc.perform(
                 get("/"))
-                //.andDo(print())
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
-                .andExpect(model().attribute("query", "123456"));
-        //.andExpect(model().attribute("sha256", "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"))
-        //.andExpect(model().attribute("md5", "e10adc3949ba59abbe56e057f20f883e"));
+                .andExpect(forwardedUrl("/WEB-INF/views/index.jsp"))
+                .andExpect(model().attribute("msg", "Hello World"));
 
+    }
+
+    @Test
+    public void testAbc() {
+        assertEquals(2, 1 + 1);
     }
 
 }
