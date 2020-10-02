4fa9b850d46cab71a299dda35c7e85121a26e520
Himanshu Gautam <himanshu@aplimasolutions.com>
2020-10-02 14:56:28 -0700

    add test config

---
+++ java-multi-modules/web/src/test/java/com/hgautam/web/config/SpringConfig.java
@@ -0,0 +1,28 @@
+package com.hgautam.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * @author Himanshu Gautam 
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.hgautam.web" })
+public class SpringConfig {
+
+	@Bean
+	public InternalResourceViewResolver resolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+}
