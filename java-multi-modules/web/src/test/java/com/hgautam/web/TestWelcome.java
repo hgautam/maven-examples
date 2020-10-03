30097cd0a728d75be80bd0f386faa15e0a747536
Himanshu Gautam <1302399+hgautam@users.noreply.github.com>
2020-10-03 11:56:07 -0700

    Update TestWelcome.java

--- java-multi-modules/web/src/test/java/com/hgautam/web/TestWelcome.java
+++ java-multi-modules/web/src/test/java/com/hgautam/web/TestWelcome.java
@@ -17,25 +17,27 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("Test Spring MVC default view")
 public class TestWelcome {
 
-	private MockMvc mockMvc;
+    private MockMvc mockMvc;
 
-	@Autowired
-	private WebApplicationContext webAppContext;
+    @Autowired
+    private WebApplicationContext webAppContext;
 
-	@BeforeEach
-	public void setup() {
-		mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
-	}
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+    }
 
-	@Test
-	public void testDefault() throws Exception {
+    @Test
+    public void testDefault() throws Exception {
 
-		this.mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"))
-				.andExpect(model().attribute("query", "123456"));
-		// .andExpect(model().attribute("sha256",
-		// "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"))
-		// .andExpect(model().attribute("md5", "e10adc3949ba59abbe56e057f20f883e"));
+        this.mockMvc.perform(
+                get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attribute("query", "123456"));
+        //.andExpect(model().attribute("sha256", "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92"))
+        //.andExpect(model().attribute("md5", "e10adc3949ba59abbe56e057f20f883e"));
 
-	}
+    }
 
 }
